b4042bfd4cc05dde6667d75b308fbedb6ff5696f
kyzgsjc1234 <1484868182@qq.com>
2018-05-01 20:42:00 +0800

    添加quillEditor

---
+++ src/components/editors/QuillEditor.vue
@@ -0,0 +1,26 @@
+<template>
+  <quill-editor v-model="value"></quill-editor>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import {quillEditor} from 'vue-quill-editor'
+
+export default {
+  name: 'wl-quill-editor',
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import MdEditor from '@/pages/mdEditor'
+import QuillEditor from '@/components/editors/QuillEditor'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/mdEditor',
       name: 'MdEditor',
       component: MdEditor
+    },
+    {
+      path: '/quillEditor',
+      name: 'QuillEditor',
+      component: QuillEditor
     }
   ]
 })
