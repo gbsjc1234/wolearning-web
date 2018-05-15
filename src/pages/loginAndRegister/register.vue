7878388e7c7af3fd9730dc8ac4d8e4c5da3ead1c
jp <1257196064@qq.com>
2018-05-15 20:33:27 +0800

    register-page

--- src/pages/loginAndRegister/register.vue
+++ src/pages/loginAndRegister/register.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="page-register">
     <div class="page-register-main">
-      <div class="page-register-login">用户注册</div>
+      <div class="page-register-title">用户注册</div>
       <Form label-position="left" :label-width="60" ref="formValidate" :rules="ruleValidate" :model="formValidate">
         <formItem label="用户名" prop="userName">
-          <i-input  v-model="formValidate.userName" placeholder="输入你的用户名" class="page-register-userName"></i-input>
+          <i-input  v-model="formValidate.userName" placeholder="输入你的用户名" class="page-register-input"></i-input>
         </formItem>
         <formItem label="昵称" prop="nickName">
-          <i-input  placeholder="输入你的昵称" v-model="formValidate.nickName"></i-input>
+          <i-input  placeholder="输入你的昵称" v-model="formValidate.nickName" class="page-register-input"></i-input>
         </formItem>
         <formItem label="密码" prop="password">
-          <i-input  placeholder="输入你的密码"  type="password" v-model="formValidate.password"></i-input>
+          <i-input  placeholder="输入你的密码"  type="password" v-model="formValidate.password" class="page-register-input"></i-input>
         </formItem>
         <formItem label="重复密码" prop="replyPassword">
-          <i-input  placeholder="重复输入你的密码"  type="password" v-model="formValidate.replyPassword"></i-input>
+          <i-input  placeholder="重复输入你的密码"  type="password" v-model="formValidate.replyPassword" class="page-register-input"></i-input>
         </formItem>
         <FormItem>
           <i-button type="primary" class="page-register-register" @click="handleSubmit('formValidate')">提交</i-button>
+          <i-button type="primary" class="page-register-login">登陆</i-button>
         </FormItem>
       </Form>
     </div>
@@ -46,7 +47,7 @@ export default {
     const validatePassword = (rule, value, callback) => {
       let regex = new RegExp('^[a-zA-Z0-9_-]{6,12}$')
       if (!regex.test(value)) {
-        callback(new Error('5-12位(字母、数字、下划线、减号)'))
+        callback(new Error('6-12位(字母、数字、下划线、减号)'))
       } else {
         callback()
       }
@@ -70,16 +71,16 @@ export default {
       },
       ruleValidate: {
         userName: [
-          {validator: validateUserName, trigger: 'blur'}
+          {validator: validateUserName, trigger: 'change'}
         ],
         nickName: [
-          {validator: validateNickName, trigger: 'blur'}
+          {validator: validateNickName, trigger: 'change'}
         ],
         password: [
-          {validator: validatePassword, trigger: 'blur'}
+          {validator: validatePassword, trigger: 'change'}
         ],
         replyPassword: [
-          {validator: validateReplyPassword, trigger: 'blur'}
+          {validator: validateReplyPassword, trigger: 'change'}
         ]
       }
     }
@@ -89,6 +90,7 @@ export default {
       this.$refs[name].validate((valid) => {
         if (valid) {
           this.$Message.success('注册成功!')
+          this.$refs[name].resetFields()
         } else {
           this.$Message.error('注册失败!')
         }
@@ -100,7 +102,7 @@ export default {
 
 <style>
   body{
-    background-color: #ddd;
+    background-color: #666;
   }
 
   .page-register {
@@ -111,23 +113,33 @@ export default {
   }
 
   .page-register-main {
-    /*border: 1px solid #F2F2F2;*/
+    /*border: 1px solid #fff;*/
     display: block;
     padding: 5px;
     background-color: #efefef;
     margin: 0 auto;
-    width: 300px;
+    width: 350px;
   }
-  .page-register-login {
+  .page-register-title {
     width: 100%;
     text-align: center;
     font: 700 16px/30px 'microsoft yahei';
     padding-bottom: 10px;
     color: #666;
   }
+
   .page-register-register {
     width: 80px;
-    height: 30px;
-    margin-left: 50px;
+    height: 28px;
+    margin-left: 45px;
   }
+
+  .page-register-login {
+    width: 80px;
+    height: 28px;
+    margin-left: 20px;
+    background-color: #a4a4a4;
+    border: #f2f2f2;
+  }
+
 </style>
